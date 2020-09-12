20403d339ac6cd1b32bd0e12d1a4295c74e0c62d
r0ma <mymail@nxt.ru>
2020-09-12 19:49:47 +0500

    Произведена начаьная настройка приложения: создан htaccess, прописаны pretty url, указано название базы данных

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'K3ogTwTHjCTK1luErDEQAxjU5RNrORoc',
+            'baseUrl' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,16 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+            	'' => 'site/index',                                
+			    '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
